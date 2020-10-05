55d1ce21f7b01ccf72d896600977c0182cd08e43
Kumar Aditya <rahuladitya303@gmail.com>
2020-10-05 18:53:58 +0530

    Formatted with black

--- pysizer.py
+++ pysizer.py
@@ -16,25 +16,60 @@ from time import perf_counter
 import click
 from PIL import Image
 
-__version__ = '1.0.0'
-__author__ = 'Kumar Aditya @kumaraditya303 '
+__version__ = "1.0.0"
+__author__ = "Kumar Aditya @kumaraditya303 "
 
 # Program start time
 t1 = perf_counter()
 
 
 @click.command()
-@click.option('--folder', default='.', show_default=True, help='pictures folder path', type=click.Path(exists=True), required=True)
-@click.option('--dest', default='resized', show_default=True, help='destination for resized pictures', type=click.Path(), required=True)
-@click.option('--height', default=1920, show_default=True, help='image height in px', type=click.INT, required=True)
-@click.option('--width', default=1280, show_default=True, help='image width in px', type=click.INT, required=True)
-@click.option('--threads', default=50, show_default=True, help='number of threads to use', type=click.INT, required=False)
+@click.option(
+    "--folder",
+    default=".",
+    show_default=True,
+    help="pictures folder path",
+    type=click.Path(exists=True),
+    required=True,
+)
+@click.option(
+    "--dest",
+    default="resized",
+    show_default=True,
+    help="destination for resized pictures",
+    type=click.Path(),
+    required=True,
+)
+@click.option(
+    "--height",
+    default=1920,
+    show_default=True,
+    help="image height in px",
+    type=click.INT,
+    required=True,
+)
+@click.option(
+    "--width",
+    default=1280,
+    show_default=True,
+    help="image width in px",
+    type=click.INT,
+    required=True,
+)
+@click.option(
+    "--threads",
+    default=50,
+    show_default=True,
+    help="number of threads to use",
+    type=click.INT,
+    required=False,
+)
 def main(folder, dest, height, width, threads):
     """
-    Main PySizer function which with ThreadPoolExecutor creates threads for 
+    Main PySizer function which with ThreadPoolExecutor creates threads for
     resizing pictures.
 
-    Checks for correct file extension, creates threads for each picture with 
+    Checks for correct file extension, creates threads for each picture with
     thread limitation as given by threads argument.
 
     Creates progress bar with the click for resizing progress.
@@ -44,23 +79,26 @@ def main(folder, dest, height, width, threads):
     :param height: the resized picture's height
     :param width: the resized picture's width
     """
-    files = [file for file in os.listdir(folder) if file.endswith(
-        '.jpg') or file.endswith('.jpeg') or file.endswith('.png')]
+    files = [
+        file
+        for file in os.listdir(folder)
+        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png")
+    ]
     if len(files) == 0:
-        click.secho('No pictures found!', fg='red')
+        click.secho("No pictures found!", fg="red")
         SystemExit(1)
     # Create thread pool with max_workers=threads
     with ThreadPoolExecutor(max_workers=threads) as executor:
-        results = [executor.submit(resize, file, dest, height, width)
-                   for file in files]
+        results = [executor.submit(resize, file, dest, height, width) for file in files]
         # Creates progress bas with current resizing progress
-        with click.progressbar(length=len(files), label='Picture resizing progress: ', color='green') as bar:
+        with click.progressbar(
+            length=len(files), label="Picture resizing progress: ", color="green"
+        ) as bar:
             for f in as_completed(results):
                 bar.update(1)
         # After execution time
         t2 = perf_counter()
-        click.secho(
-            f'Picture resizing took {round(t2-t1,2)} seconds!', fg='green')
+        click.secho(f"Picture resizing took {round(t2-t1,2)} seconds!", fg="green")
 
 
 def resize(file, resize_folder, height, width):
@@ -71,8 +109,9 @@ def resize(file, resize_folder, height, width):
         os.mkdir(resize_folder)
     i = Image.open(file)
     i.thumbnail((height, width))
-    i.save(f'{resize_folder}/{file}')
+    i.save(f"{resize_folder}/{file}")
+
 
 # Start Program
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup
 
 setup(
-	name='PySizer',
-	author='Kumar Aditya',
-	author_email='@kumaraditya303 Github',
-	description="PySizer is a simple python command line program to resize images efficiently using Threads. This program uses click as a command line argument parser. It can also be used to pyinstaller to create a executable.",
-	license="MIT",
-	keywords="Pillow ThreadPoolExecutor",
- 	url="https://github.com/kumaraditya303/PySizer",  
-	version='1.0.0',
-	py_modules=['pysizer'],
-	install_requires=[
-		"Click",
-		"Pillow",
-	],
-	entry_points='''
+    name="PySizer",
+    author="Kumar Aditya",
+    author_email="@kumaraditya303 Github",
+    description="PySizer is a simple python command line program to resize images efficiently using Threads. This program uses click as a command line argument parser. It can also be used to pyinstaller to create a executable.",
+    license="MIT",
+    keywords="Pillow ThreadPoolExecutor",
+    url="https://github.com/kumaraditya303/PySizer",
+    version="1.0.0",
+    py_modules=["pysizer"],
+    install_requires=[
+        "Click",
+        "Pillow",
+    ],
+    entry_points="""
 	[console_scripts]
 	pysizer=pysizer:main
-	''',
-	classifiers=['Topic :: Utilities'],
+	""",
+    classifiers=["Topic :: Utilities"],
 )
