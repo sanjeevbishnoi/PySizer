e7f50c3e7fc44151fb539bab7c035861737e38b3
Kumar Aditya <rahuladitya303@gmail.com>
2020-10-05 13:53:31 +0530

    Update description

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 setup(
 	name='PySizer',
+	author='Kumar Aditya',
+	author_email='@kumaraditya303 Github',
+	description="PySizer is a simple python command line program to resize images efficiently using Threads. This program uses click as a command line argument parser. It can also be used to pyinstaller to create a executable.",
+	license="MIT",
+	keywords="Pillow ThreadPoolExecutor",
+ 	url="https://github.com/kumaraditya303/PySizer",  
 	version='1.0.0',
 	py_modules=['pysizer'],
 	install_requires=[
@@ -12,4 +18,5 @@ setup(
 	[console_scripts]
 	pysizer=pysizer:main
 	''',
+	classifiers=['Topic :: Utilities'],
 )
